2f4667627ccbdcbfff2b07c69e6b7848c853347f
syrlev <epmg990@gmail.com>
2026-03-27 18:09:03 -0400

    Commit inicial: Proyecto base OTEC Primavera con dependencias

---
+++ mvp-gestion/src/main/java/cl/otecprimavera/mvp_gestion/MvpGestionApplication.java
@@ -0,0 +1,13 @@
+package cl.otecprimavera.mvp_gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpGestionApplication.class, args);
+	}
+
+}
---
+++ mvp-gestion/src/test/java/cl/otecprimavera/mvp_gestion/MvpGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.otecprimavera.mvp_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
